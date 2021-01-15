a61b005be17eaa9b657e1303050197a0157f5d67
ziko442 <ziko442@gmail.com>
2021-01-15 16:20:40 +0100

    "update snake to be inside the wall."

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from food import Food
 from scoreboard import ScoreBoard
 import time
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -16,55 +15,46 @@ food = Food()
 scoreboard = ScoreBoard()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
+
+
+def detect_collision():
+    # Detect collision with Food.
+    if snake.head.distance(food) < 10:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 9:
+            scoreboard.reset()
+            snake.reset()
+
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    
-    snake.wall_wdistense()
-        # if snake.segments[i].xcor() < -280 :
-        #     snake.segments[i].xcor(snake.segments[i].xcor() + 560)
+    snake.wall_distance(detect_collision)
+    # if snake.segments[i].xcor() < -280 :
+    #     snake.segments[i].xcor(snake.segments[i].xcor() + 560)
 
-        # if snake.segments[i].ycor() > 280 :
-        #     snake.segments[i].ycor(snake.segments[i].ycor() - 560) 
-        # if snake.segments[i].ycor() < -280 :
-        #     snake.segments[i].ycor(snake.segments[i].ycor() + 560) 
+    # if snake.segments[i].ycor() > 280 :
+    #     snake.segments[i].ycor(snake.segments[i].ycor() - 560)
+    # if snake.segments[i].ycor() < -280 :
+    #     snake.segments[i].ycor(snake.segments[i].ycor() + 560)
 
-    
-
-    #Detect collision with wall.
+    # Detect collision with wall.
     # if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-    #     game_is_on = False
-    #     scoreboard.game_over()
-
-
-    #Detect collision with Food.
-    if snake.head.distance(food) < 10:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    #Detect collision with tail.
-    for segment in snake.segments[1:]:
-
-        if snake.head.distance(segment) < 9:
-            game_is_on = False
-            scoreboard.game_over()
-
-
-
-
-
-
-
-
+    #     scoreboard.reset()
+    #     snake.reset()
+    detect_collision()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 import time
 
 screen = Screen()
-STARTING_POSITIONS = [(0, 0),(-20, 0),(-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 10
 
 RIGHT = 0
@@ -10,34 +10,38 @@ UP = 90
 LEFT = 180
 DOWN = 270
 
+
 class Snake:
 
     def __init__(self):
         self.segments = []
         self.create_snake()
-        self.head = self.segments[0]  
-
-        
+        self.head = self.segments[0]
 
     def create_snake(self):
-
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
     def add_segment(self, position):
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            new_segment.shapesize(stretch_len=0.5, stretch_wid=0.5)
-            self.segments.append(new_segment)
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        new_segment.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
-        self.add_segment(self.segments[-1].position())     
-
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.segments) - 1 , 0, -1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
@@ -47,50 +51,47 @@ class Snake:
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
-
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
-
 
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
-
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
-
-    def wall_wdistense(self):
-        #detect snace wdistense withh wall 
+    def wall_distance(self, func):
+        # detect snake dispense with wall
         if self.head.xcor() > 280:
             for seg_item in self.segments:
-                seg_item.goto(seg_item.xcor() - 560 ,seg_item.ycor()) 
+                seg_item.goto(seg_item.xcor() - 560, seg_item.ycor())
+                func()
                 screen.update()
                 time.sleep(0.1)
                 self.move()
         if self.head.xcor() < -280:
             for seg_item in self.segments:
-                seg_item.goto(seg_item.xcor() + 560 ,seg_item.ycor()) 
+                seg_item.goto(seg_item.xcor() + 560, seg_item.ycor())
+                func()
                 screen.update()
                 time.sleep(0.1)
                 self.move()
 
         if self.head.ycor() > 280:
             for seg_item in self.segments:
-                seg_item.goto(seg_item.xcor(), seg_item.ycor() - 560) 
+                seg_item.goto(seg_item.xcor(), seg_item.ycor() - 560)
+                func()
                 screen.update()
                 time.sleep(0.1)
                 self.move()
 
         if self.head.ycor() < -280:
             for seg_item in self.segments:
-                seg_item.goto(seg_item.xcor(), seg_item.ycor() + 560) 
+                seg_item.goto(seg_item.xcor(), seg_item.ycor() + 560)
+                func()
                 screen.update()
                 time.sleep(0.1)
                 self.move()
-
-    
-  
